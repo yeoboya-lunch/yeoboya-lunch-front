d9aef746b387a3dc3d0ff2f85f97afe705657b6f
Foh <forourhappy@gmail.com>
2024-02-06 14:42:03 +0900

    feat: font tailwind 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import StateProvider from './_components/StateProvider';
 import { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { cn } from '@/_lib/utils';
 
 export const metadata: Metadata = {
   title: '점심 파티 모집',
@@ -11,13 +12,14 @@ export const metadata: Metadata = {
 
 const font = localFont({
   src: './_assets/font/PretendardVariable.woff2',
+  variable: '--font-pretendard',
   display: 'swap',
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko" className={font.className}>
-      <body>
+    <html lang="ko">
+      <body className={cn('font-pretendard', font.variable)}>
         <ReactQueryProvider>
           <AuthProvider>
             <StateProvider>{children}</StateProvider>
