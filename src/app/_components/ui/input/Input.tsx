6c67058a06d64c78d0e41c7ea44203ec3ad802d4
Foh <111723390+Forourhappy@users.noreply.github.com>
2024-07-17 11:19:08 +0900

    소셜 로그인 아이콘 임시 추가 (#31)
    * feat: 로그인 페이지 소셜 로그인 경로 추가

---
+++ src/app/_components/ui/input/Input.tsx
@@ -0,0 +1,40 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+
+import { cn } from '@/app/_lib/utils';
+
+export const inputVariants = cva(
+  'flex h-9 w-full bg-transparent text-sm transition-colors file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default:
+          'px-3 py-1 rounded-md border border-input shadow-sm file:border-0 focus-visible:ring-ring focus-visible:ring-1',
+        borderNone: 'shadow-none',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, variant, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(inputVariants({ variant }), className)}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = 'Input';
+
+export { Input };
